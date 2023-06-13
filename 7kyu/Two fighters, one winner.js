33c30535e546f4abd0ddfdd6f0b83c39fdf007f3
IreneLinWasHere <heartcookies@gmail.com>
2023-06-13 09:15:43 -0700

    7kyu- Two fighters, one winner

---
+++ 7kyu/Two fighters, one winner.js
@@ -0,0 +1,45 @@
+// Create a function that returns the name of the winner in a fight between two fighters.
+
+// Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+// Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+// Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+// Your function also receives a third argument, a string, with the name of the fighter that attacks first.
+
+// Example:
+//   declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+  
+//   Lew attacks Harry; Harry now has 3 health.
+//   Harry attacks Lew; Lew now has 6 health.
+//   Lew attacks Harry; Harry now has 1 health.
+//   Harry attacks Lew; Lew now has 2 health.
+//   Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+// function Fighter(name, health, damagePerAttack) {
+//         this.name = name;
+//         this.health = health;
+//         this.damagePerAttack = damagePerAttack;
+//         this.toString = function() { return this.name; }
+
+// Solution
+function Fighter(name, health, damagePerAttack) {
+        this.name = name;
+        this.health = health;
+        this.damagePerAttack = damagePerAttack;
+        this.toString = function() { return this.name; }
+    }
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+
+    let fighter1Turns = Math.ceil(fighter2.health / fighter1.damagePerAttack)
+    let fighter2Turns = Math.ceil(fighter1.health / fighter2.damagePerAttack)
+
+    if (fighter1Turns === fighter2Turns) {
+        return firstAttacker
+    } else {
+    return fighter1Turns > fighter2Turns ? fighter2.name : fighter1.name
+    }
+}
+
+console.log(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew"))
