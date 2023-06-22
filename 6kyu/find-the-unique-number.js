4d94bff9c825e690408d1f8a7c0f133eff762b92
IreneLinWasHere <heartcookies@gmail.com>
2023-06-22 09:51:05 -0700

    6kyu- Find the Unique Number

---
+++ 6kyu/find-the-unique-number.js
@@ -0,0 +1,20 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+// Solution
+let arr = [ 1, 1, 1, 2, 1, 1 ]
+
+function findUniq(arr) {
+    return arr.find(n => {
+        return arr.indexOf(n) === arr.lastIndexOf(n)
+    })
+}
+
+
+console.log(findUniq(arr))
+  
