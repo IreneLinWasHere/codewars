fc224efab178f61cd8c2e49a545c8ce53cc93d61
IreneLinWasHere <heartcookies@gmail.com>
2023-06-28 10:59:41 -0700

    7kyu- Find the Capitals

---
+++ 7kyu/find-the-capitals.js
@@ -0,0 +1,20 @@
+// Instructions
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+// Solution
+let word = 'CodEWaRs'
+
+const capitals = word => {
+    let result = []
+    for (let i = 0; i < word.length; i++){
+        if (word[i] === word[i].toUpperCase()){
+            result.push(i)
+        }
+    }
+    return result
+}
+
+console.log(capitals('CodEWaRs'))
