4e9e0459473852181f4d9dd89e5845243f25c81b
IreneLinWasHere <heartcookies@gmail.com>
2023-07-10 11:42:49 -0700

    fixed typo

--- 6kyu/create-phone-number.js
+++ 6kyu/create-phone-number.js
@@ -8,7 +8,6 @@
 
 // Solution
 function createPhoneNumber(numbers){
-    numbers.shift()
     return `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`
 }
 
