dd4c7be644902242465186213706882455aad3b0
irenelinwashere <heartcookies@gmail.com>
2024-03-14 16:32:23 -0700

    Started refactoring with arrow functions

--- 5kyu/calculating-with-functions.js
+++ 5kyu/calculating-with-functions.js
@@ -13,18 +13,56 @@
 // Division should be integer division. For example, this should return 2, not 2.666666...:
 // eight(dividedBy(three()));
 
-function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+const zero = fn => fn ? fn(0) : 0
+const one = fn => fn ? fn(1) : 1
 
-function plus() {}
-function minus() {}
-function times() {}
-function dividedBy() {}
+
+
+function two(operation) {
+    return operation ? operation(2) : 2;
+}
+function three(operation) {
+    return operation ? operation(3) : 3;
+}
+function four(operation) {
+    return operation ? operation(4) : 4;
+}
+function five(operation) {
+    return operation ? operation(5) : 5;
+}
+function six(operation) {
+    return operation ? operation(6) : 6;
+}
+function seven(operation) {
+    return operation ? operation(7) : 7;
+}
+function eight(operation) {
+    return operation ? operation(8) : 8;
+}
+function nine(operation) {
+    return operation ? operation(9) : 9;
+}
+
+function plus(x) {
+    return function (y) {
+        return x + y
+    }
+
+}
+function minus(x) {
+    return function (y) {
+        return x - y
+    }
+}
+function times(x) {
+    return function (y) {
+        return x * y
+    }
+}
+function dividedBy(x) {
+    return function (y) {
+        return Math.floor(y / x)
+    }
+}
+
+console.log(four(plus(nine())))
